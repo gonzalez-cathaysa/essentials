5fef73d5534a7cf8bd48acf59726f83725e6bc61
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-09-26 16:03:38 +0200

    Defining Component Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,13 +11,28 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //Property settable from outside
+  @Input() avatar!:string;
+  @Input() name!: string;
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath=computed(()=>'assets/users/'+this.selectedUser().avatar)
+  // // get imagePath(){
+  // //   return 'assets/users/'+this.selectedUser.avatar
+  // // }
 
   get imagePath(){
-    return 'assets/users/'+this.selectedUser.avatar
+    return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(){
-    console.log('Clicked');
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
+
+
+function Signal(arg0: { id: string; name: string; avatar: string; }) {
+  throw new Error('Function not implemented.');
+}
+
