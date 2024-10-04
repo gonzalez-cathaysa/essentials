48566c59bc160524d8df37f914cf8051613a9d72
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-04 14:06:50 +0200

    Transforming Template Data with Pipes

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-task',
     standalone: true,
     templateUrl: './task.component.html',
     styleUrl: './task.component.css',
-    imports: [CardComponent]
+    imports: [CardComponent, DatePipe]
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
